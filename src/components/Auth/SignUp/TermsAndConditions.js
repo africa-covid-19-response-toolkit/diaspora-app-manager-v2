ffeb39ea93f1c26bbe69abd807e9dfb1e78f7c70
Helen Gezahegn <gezahegn@ualberta.ca>
2020-05-25 03:24:23 -0600

    feat: forgot password + terms/conds placeholders

---
+++ src/components/Auth/SignUp/TermsAndConditions.js
@@ -0,0 +1,60 @@
+import React, { Component, Fragment } from "react";
+import { Modal, Button, Checkbox } from 'antd';
+
+export const termsAndConditions = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+class TermsAndConditions extends Component {
+  state = {
+    ModalText: termsAndConditions, 
+    visible: false,
+    confirmLoading: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = () => {
+    this.setState({
+      confirmLoading: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        visible: false,
+        confirmLoading: false,
+      });
+    }, 500);
+  };
+
+  handleCancel = () => {
+    console.log('Clicked cancel button');
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    const { visible, confirmLoading, ModalText } = this.state;
+    return (
+      <Fragment>
+        <Checkbox>
+          I have read the 
+          <Button onClick={this.showModal}> terms and conditions</Button>
+        </Checkbox>
+        <Modal
+          title="Terms and Conditions"
+          visible={visible}
+          onOk={this.handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
+        >
+          <p>{ModalText}</p>
+        </Modal>
+      </Fragment>
+    );
+  }
+}
+
+export default TermsAndConditions;
