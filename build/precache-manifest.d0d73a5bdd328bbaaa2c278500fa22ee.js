ebd6a60fc4834205dcf5ae13ad7f05e5ff623b27
Helen Gezahegn <gezahegn@ualberta.ca>
2020-05-25 05:52:05 -0600

    feat: added firebase deployment

---
+++ build/precache-manifest.d0d73a5bdd328bbaaa2c278500fa22ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257d765d9cac0f3e6c5ae278f3121029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "899e007db0a113f93ba2",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "0846e6636927c5cbc2c3",
+    "url": "/static/css/main.5db5b2d9.chunk.css"
+  },
+  {
+    "revision": "899e007db0a113f93ba2",
+    "url": "/static/js/2.6cae8cd7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6cae8cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0846e6636927c5cbc2c3",
+    "url": "/static/js/main.75307f30.chunk.js"
+  },
+  {
+    "revision": "8909687764ad036708a5",
+    "url": "/static/js/runtime-main.13c5f4d8.js"
+  }
+]);
