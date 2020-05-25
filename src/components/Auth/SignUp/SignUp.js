2c9f897dee5ef44f82793aa22acaea0a058b2e9a
Helen Gezahegn <gezahegn@ualberta.ca>
2020-05-25 03:25:17 -0600

    fix: delete unused imported component

--- src/components/Auth/SignUp/SignUp.js
+++ src/components/Auth/SignUp/SignUp.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  Form,
-  Input,
-  Checkbox,
-  Button
-} from 'antd';
+import { Form, Input, Button } from 'antd';
 import TermsAndConditions from "./TermsAndConditions";
 
 const SignUp = () => {
