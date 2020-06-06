76e3707722186aff206ec2b7aee4349349e2af7f
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-06 03:15:37 -0600

    chore: delete unused imports

--- src/components/Questions/QuestionActions.js
+++ src/components/Questions/QuestionActions.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { ButtonIcon } from 'react-rainbow-components';
