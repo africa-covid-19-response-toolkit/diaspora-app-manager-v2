d31b953434552fa7dfbe86fb97aea012a6987e38
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-06 05:03:31 -0600

    feat: render the 4 input fields for the languages

--- src/components/Questions/Actions/EditActionDrawer.js
+++ src/components/Questions/Actions/EditActionDrawer.js
@@ -1,23 +1,70 @@
 import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-import { Drawer, Button } from "react-rainbow-components";
+import { Drawer, Button, Input } from "react-rainbow-components";
 
 const FooterContainer = styled.section`
     display: flex;
     justify-content: space-between;
 `;
 
-const EditActionDrawer = ({ editActionDrawerVisible, dispatch }) => {
+const Subheader = styled.span`
+    font-family: Lato;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 1.06;
+    margin: 7px;
+    display: block;
+`;
+
+const inputStyles = {
+    width: 300,
+};
+
+const EditActionDrawer = ({ 
+    editActionDrawerVisible, 
+    questionSelected, 
+    dispatch 
+}) => {
 
     const renderForm = () => {
-        console.log(editActionDrawerVisible);
         return(
-            <form id="redux-form-id" noValidate >
-                Content
+            <form>
+                <Input
+                    label="English"
+                    type="text"
+                    className="rainbow-p-around_medium"
+                    style={inputStyles}
+                />
+                <Input
+                    label="Amharic"
+                    type="text"
+                    className="rainbow-p-around_medium"
+                    style={inputStyles}
+                />
+                <Input
+                    label="Oromo"
+                    type="text"
+                    className="rainbow-p-around_medium"
+                    style={inputStyles}
+                />
+                <Input
+                    label="Tigriyna"
+                    type="text"
+                    className="rainbow-p-around_medium"
+                    style={inputStyles}
+                />
             </form>
         )
     };
+
+    const renderHeader = () => {
+        return(
+            <Subheader>
+                Edit and translate the following actions.
+            </Subheader>
+        )
+    }
 
     const handleClose = () => {
         dispatch({
@@ -50,21 +97,24 @@ const EditActionDrawer = ({ editActionDrawerVisible, dispatch }) => {
     };
 
     return (
-      <Drawer
-        header="Edit action"
-        slideFrom="right"
-        isOpen={editActionDrawerVisible}
-        onRequestClose={handleClose}
-        footer={renderFooter()}
-      >
-          {renderForm()}
-      </Drawer>
-
+        <div className="rainbow-p-around_small rainbow-flex rainbow-flex_wrap  rainbow-align-content_center">
+            `<Drawer
+                header="Edit action"
+                slideFrom="right"
+                isOpen={editActionDrawerVisible}
+                onRequestClose={handleClose}
+                footer={renderFooter()}
+            >
+                {questionSelected ? renderHeader() : null}
+                {questionSelected ? renderForm() : null}
+            </Drawer>`
+      </div>
     )
 }
 
 const mapStateToProps = state => {
     return { 
+        questionSelected: state.editActionDrawerVisible, 
         editActionDrawerVisible: state.editActionDrawerVisible
     }
 };
