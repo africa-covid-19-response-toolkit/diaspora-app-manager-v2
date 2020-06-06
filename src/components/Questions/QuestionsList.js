2d9f66af2b028946059f31428f3fad4a70f689a5
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-05 20:30:36 -0600

    chore: unmounted error still exists...

--- src/components/Questions/QuestionsList.js
+++ src/components/Questions/QuestionsList.js
@@ -22,8 +22,8 @@ const filterQuestions = (questions) => {
 
 const QuestionsList = () => {
   const [questions, setQuestions] = useState([]);
-  
-  const getQuestions = async (unmounted) => {
+
+  useEffect(() => {
     const db = firebase.firestore();
     connectEmulatorToApp(db); 
 
@@ -36,20 +36,8 @@ const QuestionsList = () => {
               ...doc.data()
           }));
           allQuestions = filterQuestions(allQuestions);
-          if (!unmounted)
-            setQuestions(allQuestions);
+          setQuestions(allQuestions);
       });
-  }
-
-  useEffect(() => {
-    let unmounted = false;
-    async function fetchData() {
-      await getQuestions(unmounted);
-    }
-    fetchData();
-    return () => {
-      unmounted = true
-    }
   }, []);
 
   const renderTableActions = () => {
