ccdd1e8bd6ca0e3ff1f8a63b7f8eec6dfbd734a6
Helen Gezahegn <gezahegn@ualberta.ca>
2020-06-17 21:01:57 -0600

    feat: prettify JSON logic

--- src/components/Questions/Actions/EditNextQuestionField.js
+++ src/components/Questions/Actions/EditNextQuestionField.js
@@ -7,13 +7,16 @@ const containerStyles = {
 
 const EditNextQuestionField = ({ questionSelected }) => {
     const { next } = questionSelected;
+    const nextJson = JSON.parse(next);
+
+    const prettifyJson = () => JSON.stringify(nextJson, undefined, 4);
+
     return(
         <Textarea
             rows={5}
-            value={next}
+            value={prettifyJson()}
             style={containerStyles}
             label="Edit the Next Question"
-            bottomHelpText="Edit the JSON logic used to retrieve the next question"
             className="rainbow-m-vertical_x-large rainbow-p-horizontal_medium rainbow-m_auto"
         />
     );
